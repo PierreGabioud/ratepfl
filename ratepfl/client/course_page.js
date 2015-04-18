42b095b9cb417b9ceb9ea8da5902da8e253242e4
Pierre Gabioud <pierregabioud@gmail.com>
2015-04-18 19:29:50 +0200

    donnne

--- ratepfl/client/course_page.js
+++ ratepfl/client/course_page.js
@@ -12,9 +12,34 @@ Template.coursePage.helpers({
 	},
 	commentsNb : function(){
 		return Comments.find({courseID: this._id}).count()
+	},
+	oneToTen: function(){
+		return [1,2,3,4,5,6,7,8,9,10];
+	},
+	getNbHoursColor: function(){
+		return (Number(this) <= Session.get("nbHoursRating")) ? "red-text text-darken-3" : "black-text";
+	},
+	getTeacherRatingColor: function(){
+		// console.log(Number(this)+" vs "+Session.get("teacherRating"));
+		return (Number(this) <= Session.get("teacherRating")) ? "green-text text-darken-3" : "black-text";
 	}
 });
 
+
+Template.coursePage.events({
+	'mouseover .nbhoursrating': function (e) {
+		//console.log($(e.target).data("nbhours"));
+		// console.log("hoer");
+		Session.set("nbHoursRating", $(e.target).data("nbhoursrating"));
+	},
+	'mouseover .teacherRating': function(e){
+		// console.log($(e.target).data("teacherrating"));
+		Session.set("teacherRating", $(e.target).data("teacherrating"));
+	},
+	'click .teacherRating': function(e){
+		console.log($(e.target).data("teacherrating"));
+	}
+});
 Template.coursePage.events(
 	{
 		"submit .new-comment": function(event)
