e5a702284bff73cadad2d4dfa74ec4cf0613c854
Pierre Gabioud <pierregabioud@gmail.com>
2015-04-18 21:59:57 +0200

    dsf

--- ratepfl/lib/collections/rating.js
+++ ratepfl/lib/collections/rating.js
@@ -2,14 +2,26 @@ Ratings = new Mongo.Collection('ratings');
 
 Meteor.methods({
 
-	changeRating : function(){
+	changeRatingTeaching : function(ratedID, newRating){
 		console.log("Changing rating");
 		if(!Meteor.userId()){
 			console.log("Need to be connected");
 		}else{
 
-			console.log("Updating rating to of this dude on this ratedID");
+			console.log("Updating rating to of this dude "+Meteor.userId()+"on this ratedID "+ratedID);
 
+			Ratings.update({userID: Meteor.userId(), ratedID: ratedID}, {$set: {ratingTeacher: newRating}});
+		}
+	},
+	changeRatingHours : function(ratedID, newRating){
+		console.log("Changing rating of nb hours");
+		if(!Meteor.userId()){
+			console.log("Need to be connected");
+		}else{
+
+			console.log("Updating rating to of this dude "+Meteor.userId()+"on this ratedID "+ratedID);
+
+			Ratings.update({userID: Meteor.userId(), ratedID: ratedID}, {$set: {ratingHours: newRating}});
 		}
 	}
 
