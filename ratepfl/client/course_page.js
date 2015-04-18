06cf8bf80b93fed27dc9f7f131e995ecaafd932d
Pierre Gabioud <pierregabioud@gmail.com>
2015-04-18 18:36:04 +0200

    dsf

--- ratepfl/client/course_page.js
+++ ratepfl/client/course_page.js
@@ -14,3 +14,22 @@ Template.coursePage.helpers({
 		return Comments.find({courseID: this._id}).count()
 	}
 });
+
+Template.coursePage.events(
+	{
+		"submit .new-comment": function(event)
+		{
+			var courseID = this.course._id;
+			var comment = event.target.comment.value;
+			var timestamp = new Date().getTime();
+
+			// send add request to the server
+			Meteor.call("newComment", courseID, comment, timestamp);
+
+			// empty form
+			event.target.reset();
+			$('#modal-add-comment').closeModal();
+
+			return false;
+		}
+	})
