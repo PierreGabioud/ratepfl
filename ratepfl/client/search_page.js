f7b5287c8c46d7e3d02e559ee1b275d32f9dc597
mrudelle <matthieu.rudelle@gmail.com>
2015-04-19 07:36:46 +0200

    working searching algo

--- ratepfl/client/search_page.js
+++ ratepfl/client/search_page.js
@@ -1,9 +1,27 @@
 Template.searchPage.helpers({
     results: function () {
-        return Courses.find({}).fetch().splice(0, Session.get('query').length)
+
+        var ret = _.filter(Courses.find({}).fetch(), function(elem){return match(Session.get('query'), elem.title)})
+
+        return ret
     },
 });
 
-Template.searchPage.events({
+var match  = function(search, text) {
 
-});
+    console.log(search, text)
+    search = search.toUpperCase();
+    text = text.toUpperCase();
+
+    var j = -1; // remembers position of last found character
+
+    // consider each search character one at a time
+    for (var i = 0; i < search.length; i++) {
+        var l = search[i];
+        if (l == ' ') continue;     // ignore spaces
+
+        j = text.indexOf(l, j+1);     // search for character & update position
+        if (j == -1) return false;  // if it's not found, exclude this item
+    }
+    return true;
+}
